b15c239cd6f41348c0e472b55297fa441f90c66e
BaranovMykola <mapsg32@gmail.com>
2017-02-15 15:55:39 +0200

    Reverse all negative elements in  array

--- C-Sharp Eductional Practice/Program.cs
+++ C-Sharp Eductional Practice/Program.cs
@@ -10,6 +10,40 @@ namespace C_Sharp_Eductional_Practice
     {
         static void Main(string[] args)
         {
+            string arrStr = Console.ReadLine();
+            string[] arrStrSpit = arrStr.Split(' ');
+            int[] arr = new int[arrStrSpit.Length];
+            int index = 0;
+            foreach(string i in arrStrSpit)
+            {
+                arr[index++] = int.Parse(i);
+            }
+            int f = 0;
+            int s = arrStrSpit.Length - 1;
+            do
+            {
+                while(arr[f] > 0)
+                {
+                    ++f;
+                }
+                while (arr[s] > 0)
+                {
+                    --s;
+                }
+                int t = arr[f];
+                arr[f] = arr[s];
+                arr[s] = t;
+                ++f;
+                --s;
+            }
+            while (f < s);
+               foreach(int i in arr)
+            {
+                Console.Write(i);
+                Console.Write(' ');
+            }
+            Console.Write('\n');
+            Console.ReadLine();
         }
     }
 }
